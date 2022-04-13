15378163c3f4cb5bf2de7fe9229d74af2b08884b
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-04-13 19:31:30 -0300

    Add files via upload

---
+++ java_poo/Book/src/main/java/com/mycompany/book/BookMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.book;
+
+/**
+ *
+ * @author franco
+ */
+public class BookMain {
+
+    public static void main(String[] args) {
+
+        Libro l1 = new Libro();
+
+        l1.getDatos();
+        l1.mostrarResultado();
+    }
+}
