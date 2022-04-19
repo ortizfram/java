7d0213c8d77823b04f15e92b7b5acfc3a5985607
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-04-19 10:29:34 -0300

    Add files via upload

--- java_poo/cuentaBancaria/src/main/java/Serivcio/CuentaBancServicio.java
+++ java_poo/cuentaBancaria/src/main/java/Serivcio/CuentaBancServicio.java
@@ -21,21 +21,21 @@ public class CuentaBancServicio {
 
     public void Ingresar(CuentaBancaria c1, float salgoActual) {
         Scanner leer = new Scanner(System.in);
-        System.out.println("Monto a INGRESAR $$$");
+        System.out.println("Monto a INGRESAR +$$$");
         c1.setIngreso(leer.nextFloat());
-        c1.setIngreso(c1.getSalgoActual() + c1.getIngreso());
-        System.out.println("Saldo de : " + c1.getIngreso());
+        c1.setSalgoActual(c1.getIngreso() + c1.getSalgoActual());
+        System.out.println("Saldo de : " + c1.getSalgoActual());
     }
 
-    public void Retirar(CuentaBancaria c1, float salgoActual) {
+    public void Retirar(CuentaBancaria c1, float salgoActual, float egreso) {
         Scanner leer = new Scanner(System.in);
         System.out.println("Monto a RETIRAR -$$$");
         c1.setEgreso(leer.nextFloat());
         if (c1.getSalgoActual() > 0) {
-            c1.setEgreso(c1.getSalgoActual() - c1.getEgreso());
-            System.out.println("Saldo de : " + c1.getEgreso());
+            c1.setSalgoActual(c1.getSalgoActual() - c1.getEgreso());
+            System.out.println("Saldo de : " + c1.getSalgoActual());
         } else {
-            System.out.println("Saldo = 0");
+            System.out.println("**imposible : Saldo = 0");
         }
     }
 
@@ -44,35 +44,35 @@ public class CuentaBancServicio {
      * permitirá sacar solo un 20% de su saldo. Validar que el usuario no saque
      * más del 20%.
      */
-    public void ExtraccionRapida(CuentaBancaria c1, float salgoActual) {
+    public void ExtraccionRapida(CuentaBancaria c1, float interes, float salgoActual) {
         /**
          * porcentaje : egreso * 100 / saldoActual;
          */
         System.out.println("Monto a retirar : ");
         c1.setEgreso(leer.nextFloat());
         c1.setInteres((c1.getEgreso() * 100) / c1.getSalgoActual());
-        if (c1.getEgreso() <= 20) {
+        if (c1.getInteres() <= 20) {
             System.out.println("***egreso menor al 20%, no es posible" + "\n"
                     + "realizar la extraccion");
         } else {
             System.out.println("Interes de Extraccion R: " + c1.getInteres());
             if (c1.getSalgoActual() > 0) {
-                c1.setEgreso(c1.getSalgoActual() - c1.getEgreso());
-                System.out.println("Saldo de : " + c1.getEgreso());
+                c1.setSalgoActual(c1.getSalgoActual() - c1.getEgreso());
+                System.out.println("Saldo de : " + (c1.getSalgoActual() - c1.getInteres()));
             } else {
-                System.out.println("Saldo = 0");
+                System.out.println("**imposible : Saldo = 0");
             }
         }
     }
 
     public void mostrarSaldo(CuentaBancaria c1, float salgoActual) {
-        System.out.println("Su saldo es de : " + c1.getEgreso());
+        System.out.println("Su saldo es de : " + c1.getSalgoActual());
     }
 
     public void mostrarDatos(CuentaBancaria c1, int DNI, int numeroCuenta, float salgoActual) {
         System.out.println("DNI : " + c1.getDNI());
         System.out.println("numero Cuenta : " + c1.getNumeroCuenta());
-        System.out.println("Saldo : " + c1.getEgreso());
+        System.out.println("Saldo : " + c1.getSalgoActual());
     }
 
 }
--- java_poo/cuentaBancaria/src/main/java/main/CuentaBancariaMain.java
+++ java_poo/cuentaBancaria/src/main/java/main/CuentaBancariaMain.java
@@ -5,45 +5,55 @@ import Serivcio.CuentaBancServicio;
 import java.util.Scanner;
 
 public class CuentaBancariaMain {
-
+    
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         CuentaBancServicio cs = new CuentaBancServicio();
         CuentaBancaria c1 = cs.crearCuenta();
-
+        
         int opcion = 0;
-
+        
         cs.Ingresar(c1, 0);
         System.out.println("-----------\t");
-        System.out.println("Ingrese del 1 al 5 " + "\n"
-                + "1 = Ingreso" + "\n"
-                + "2 = Retiro" + "\n"
-                + "3 = Extracc Rapida" + "\n"
-                + "4 = mostrar Saldo" + "\n"
-                + "5 = mostrar Datos" + "\t"
-                + "F = SALIR");
-        opcion = leer.nextInt();
-        switch (opcion) {
-            case 1:
-                cs.Ingresar(c1, 0);
-                System.out.println("---------\t");
-
-            case 2:
-                cs.Retirar(c1, 0);
-                System.out.println("---------\t");
-
-            case 3:
-                cs.ExtraccionRapida(c1, 0);
-                System.out.println("----------\t");
-
-            case 4:
-                cs.mostrarSaldo(c1, 0);
-                System.out.println("----------\t");
-
-                case5:
-                cs.mostrarDatos(c1, 0, 0, 0);
-                System.out.println("----------\t");
-        }
-
+        do {
+            System.out.println("Ingrese del 1 al 5 " + "\n"
+                    + "1 = Ingreso" + "\n"
+                    + "2 = Retiro" + "\n"
+                    + "3 = Extracc Rapida" + "\n"
+                    + "4 = mostrar Saldo" + "\n"
+                    + "5 = mostrar Datos" + "\t"
+                    + "6 = SALIR");
+            opcion = leer.nextInt();
+            
+            switch (opcion) {
+                case 1:
+                    cs.Ingresar(c1, opcion);
+                    System.out.println("---------\t");
+                    break;
+                
+                case 2:
+                    cs.Retirar(c1, opcion, opcion);
+                    System.out.println("---------\t");
+                    break;
+                
+                case 3:
+                    System.out.println("Extraccion Rapida");
+                    cs.ExtraccionRapida(c1, opcion, opcion);
+                    System.out.println("----------\t");
+                    break;
+                
+                case 4:
+                    cs.mostrarSaldo(c1, opcion);
+                    System.out.println("----------\t");
+                    break;
+                
+                case 5:
+                    cs.mostrarDatos(c1, opcion, opcion, opcion);
+                    System.out.println("----------\t");
+                    break;
+            }
+        } while (opcion != 6);
+        System.out.println("OPERACION FINALIZADA");
+        
     }
 }
