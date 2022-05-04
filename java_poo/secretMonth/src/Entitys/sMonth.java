83935f703d2ee3e31796dfbf440c92f33cc6bcdc
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-05-04 20:50:19 -0300

    Add files via upload

---
+++ java_poo/secretMonth/src/Entitys/sMonth.java
@@ -0,0 +1,13 @@
+package Entitys;
+
+public class sMonth {
+    final String[] month = {"january", "february", "march", "april", "may", "august", "september", "october", "november", "december"};
+    final String secretMonth = month[(int) (Math.random() * 12)];
+
+    public String[] getMes() {
+        return month;
+    }
+    public String getMesSecreto() {
+        return secretMonth;
+    }
+}
